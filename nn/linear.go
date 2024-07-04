ea8ca2b2185941e3451d60821f65a6c805d282d5
Eduardo Montilva <edumntg@gmail.com>
2024-07-04 00:53:28 -0400

    Rewritted Tensor class

--- nn/linear.go
+++ nn/linear.go
@@ -1,32 +1,48 @@
 package nn
 
-import . "go-nn/tensor"
+import (
+	. "go-nn/tensor"
+	"math/rand"
+)
 
 type Linear struct {
-	N []*Neuron // a layer is full of neurons
+	//N []*Neuron // a layer is full of neurons
+	W *Tensor
+	B *Tensor
+}
+
+// func NewLinear(in_size, out_size int) *Linear {
+// 	n_array := make([]*Neuron, out_size)
+// 	for i := 0; i < out_size; i++ {
+// 		n_array[i] = NewNeuron(in_size)
+// 	}
+
+// 	return &Linear{N: n_array}
+// }
+
+func New1DRandomFloatArray(size int) []float64 {
+	arr := make([]float64, size)
+	for i := 0; i < size; i++ {
+		arr[i] = rand.Float64()
+	}
+
+	return arr
 }
 
 func NewLinear(in_size, out_size int) *Linear {
-	n_array := make([]*Neuron, out_size)
-	for i := 0; i < out_size; i++ {
-		n_array[i] = NewNeuron(in_size)
-	}
+	// Initialize weights
+	var W *Tensor = RandomTensor([]int{in_size, out_size})
+	var B *Tensor = RandomTensor([]int{1, out_size})
 
-	return &Linear{N: n_array}
+	return &Linear{W: W, B: B}
 }
 
-func (l *Linear) Forward(x []*Tensor) []*Tensor {
-	out_arr := make([]*Tensor, len(l.N))
-
-	for i := 0; i < len(l.N); i++ {
-		out_arr[i] = (l.N[i]).Forward(x)
-	}
-
-	return out_arr
+func (l *Linear) Forward(x *Tensor) *Tensor {
+	return l.W.Mul(x).Add(l.B) // x@w + b
 }
 
-func (l *Linear) Print() {
-	for i := 0; i < len(l.N); i++ {
-		l.N[i].Print()
-	}
-}
+// func (l *Linear) Print() {
+// 	for i := 0; i < len(l.W); i++ {
+// 		l.W[i].Print()
+// 	}
+// }
--- tensor/tensor.go
+++ tensor/tensor.go
@@ -1,84 +1,99 @@
 package tensor
 
-import "math/rand"
+import (
+	"math/rand"
+)
 
 type Tensor struct {
-	value      float64
-	grad       float64
-	prev       []Tensor
-	_grad_func func()
+	value         [][]float64
+	grad          [][]float64
+	shape         []int
+	prev          []Tensor
+	_grad_func    func()
+	requires_grad bool
 }
 
-func NewTensor(value float64) *Tensor {
+func NewTensor(value [][]float64) *Tensor {
 	return &Tensor{
 		value: value,
+		shape: []int{len(value), len(value[0])},
 	}
 }
 
-func RandomTensor() *Tensor {
+func RandomTensor(shape []int) *Tensor {
+	value_arr := make([][]float64, shape[0])
+	for i := 0; i < shape[0]; i++ {
+		value_arr[i] = make([]float64, shape[1])
+		for j := 0; j < shape[1]; j++ {
+			value_arr[i][j] = rand.Float64()
+		}
+	}
+
 	return &Tensor{
-		value: rand.Float64(),
+		value: value_arr,
+		shape: shape,
 	}
 }
 
-func NewTensorArray(value []float64) []*Tensor {
-	tensor_arr := make([]*Tensor, len(value))
-
-	for i := 0; i < len(value); i++ {
-		tensor_arr[i] = NewTensor(value[i])
-	}
-
-	return tensor_arr
-}
-
-func RandomTensorArray(size int) []*Tensor {
-	tensor_arr := make([]*Tensor, size)
-
-	for i := 0; i < size; i++ {
-		tensor_arr[i] = RandomTensor()
-	}
-
-	return tensor_arr
-}
-
-func (t *Tensor) Get() float64 {
+func (t *Tensor) Get() [][]float64 {
 	return t.value
 }
 
+func (t *Tensor) Set(row, column int, value float64) {
+	t.value[row][column] = value
+}
+
+func (t *Tensor) Shape() []int {
+	return t.shape
+}
+
 func (t *Tensor) Add(other *Tensor) *Tensor {
-	out := &Tensor{value: (t.value + other.value), prev: []Tensor{*t, *other}}
+
+	out_arr := make([][]float64, t.shape[0])
+	for i := 0; i < t.shape[0]; i++ {
+		out_arr[i] = make([]float64, t.shape[1])
+		for j := 0; j < t.shape[1]; j++ {
+			out_arr[i][j] = t.value[i][j] + other.value[i][j]
+		}
+	}
 
 	// Declare the gradient function. This is the function used to calculate the gradient for a + operation
 	_grad_func := func() {
-		t.grad += out.grad
-		other.grad += out.grad
+		for i := 0; i < t.shape[0]; i++ {
+			for j := 0; j < t.shape[1]; j++ {
+				t.grad[i][j] += out_arr[i][j]
+				other.grad[i][j] += out_arr[i][j]
+			}
+		}
 	}
 
-	out._grad_func = _grad_func
-	return out
+	return &Tensor{
+		value:      out_arr,
+		_grad_func: _grad_func,
+	}
 }
 
 func (t *Tensor) Mul(other *Tensor) *Tensor {
-	out := &Tensor{value: (t.value * other.value), prev: []Tensor{*t, *other}}
 
-	_grad_func := func() {
-		t.grad = out.grad * other.value
-		other.grad = out.grad * t.value
+	out_arr := make([][]float64, t.shape[0])
+	for i := 0; i < t.shape[0]; i++ {
+		out_arr[i] = make([]float64, other.shape[1])
 	}
 
-	out._grad_func = _grad_func
-	return out
-}
+	// APply multiplication
+	total := float64(0.0)
+	for i := 0; i < t.shape[0]; i++ {
+		for j := 0; j < other.shape[1]; j++ {
+			for k := 0; k < t.shape[1]; k++ {
+				total += t.value[i][k] * other.value[k][j]
+			}
 
-func (t *Tensor) Div(other *Tensor) *Tensor {
-	out := &Tensor{value: (t.value / other.value), prev: []Tensor{*t, *other}}
-
-	_grad_func := func() {
-		t.grad = out.grad / other.value
-		other.grad = out.grad / t.value
+			out_arr[i][j] = total
+			total = 0.0
+		}
 	}
 
-	out._grad_func = _grad_func
-
-	return out
+	return &Tensor{
+		value: out_arr,
+	}
 }
